f205256a5abfb37d2c437e658f0a76d1916ef671
Maxwell paulo <maxpaulo.mps@gmail.com>
2026-05-22 15:52:17 -0300

    garantir salvar informações de acesso

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,19 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
+function createClient(): PrismaClient {
+  const connectionString = process.env.DATABASE_URL;
+  if (!connectionString) throw new Error("DATABASE_URL não definida");
+
+  const adapter = new PrismaPg({ connectionString });
+  return new PrismaClient({ adapter });
+}
+
 function getDb(): PrismaClient {
   if (!globalForPrisma.prisma) {
-    globalForPrisma.prisma = new PrismaClient();
+    globalForPrisma.prisma = createClient();
   }
   return globalForPrisma.prisma;
 }
